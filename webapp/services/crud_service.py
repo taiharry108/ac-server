d311612de7c893004353fea6be0f742d6cd32ea9
Harry Chan <taiharry108@gmail.com>
2022-02-25 21:48:54 -0700

    update database with async orm

--- webapp/services/crud_service.py
+++ webapp/services/crud_service.py
@@ -1,12 +1,15 @@
 import dataclasses
 from logging import getLogger
-from typing import Any, Callable, Dict, List
-from webapp.models.db_models import User
+from typing import Any, Dict, List, Union
 from webapp.services.database import Database
 
 from typing import List, TypeVar, Type
 from functools import wraps
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession as Session
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import selectinload
+from webapp.models.db_models import favorite_table
 
 T = TypeVar("T")
 
@@ -15,9 +18,10 @@ logger = getLogger(__name__)
 
 def add_session(func):
     @wraps(func)
-    def wrapped(self, *args, **kwargs):
-        with self.database.session() as session:
-            return func(self, session, *args, **kwargs)
+    async def wrapped(self, *args, **kwargs):
+        async with self.database.session() as session:
+            async with session.begin():
+                return await func(self, session, *args, **kwargs)
     return wrapped
 
 
@@ -44,159 +48,151 @@ def rearrange_items(og_items, shuffled_items, unique_key, og_is_value=False, kee
 class CRUDService:
     database: Database
 
-    @add_session
-    def get_item_by_id(self, session: Session, orm_obj_type: Type[T], id: int) -> T:
+    async def get_item_by_id(self, session: Session, orm_obj_type: Type[T], id: int, preload_child_name: str = None) -> T:
         """Get item by id, return None if not exists"""
-        query_result = session.query(orm_obj_type).get(id)
-        if not query_result:
+        q = select(orm_obj_type).where(getattr(orm_obj_type, "id") == id)
+        if preload_child_name:
+            q = q.options(selectinload(getattr(orm_obj_type, preload_child_name)))
+        try:
+            result = (await session.execute(q)).one()
+        except NoResultFound:
             return None
-        return query_result
-
-    @add_session
+        return result[0]
+         
     def get_attr_of_item_by_id(self, session: Session, orm_obj_type: Type[T], id: int, attr_name: str):
-        return getattr(session.query(orm_obj_type).get(id), attr_name)
+        db_item = self.get_item_by_id(session, orm_obj_type, id)
+        return getattr(db_item, attr_name)
 
-    @add_session
     def get_items_by_ids(self, session: Session, orm_obj_type: Type[T], ids: List[int], keep_none=False) -> List[T]:
         """Get item by id, return empty list if not exists"""
         result = session.query(orm_obj_type).filter(
             orm_obj_type.id.in_(ids)).all()
         return rearrange_items(ids, result, "id", True, keep_none=keep_none)
 
-    def get_item_by_attr(self, orm_obj_type: Type[T], attr_name: str, attr_value: Any) -> T:
-        return self.get_item_by_attrs(orm_obj_type, **{attr_name: attr_value})
+    async def get_item_by_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_value: Any) -> T:
+        return await self.get_item_by_attrs(session, orm_obj_type, **{attr_name: attr_value})
 
-    @add_session
-    def get_item_by_attrs(self, session: Session, orm_obj_type: Type[T], **kwargs) -> T:
-        q = session.query(orm_obj_type)
+    async def get_item_by_attrs(self, session: Session, orm_obj_type: Type[T], **kwargs) -> T:
+        q = select(orm_obj_type)
+        for attr_name, attr_value in kwargs.items():
+            q = q.where(getattr(orm_obj_type, attr_name) == attr_value)
+        result = await session.execute(q)
+        (result, ) = result.one()
+        return result
 
-        filter_query = [getattr(orm_obj_type, attr_name) == attr_value
-                        for attr_name, attr_value in kwargs.items()]
-        return q.filter(*filter_query).first()
+    async def get_id_by_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_value: Any) -> int:
+        return (await self.get_item_by_attr(session, orm_obj_type, attr_name, attr_value)).id
 
-    def get_id_by_attr(self, orm_obj_type: Type[T], attr_name: str, attr_value: Any) -> int:
-        return self.get_item_by_attr(orm_obj_type, attr_name, attr_value).id
-
-    @add_session
-    def get_items_by_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_values: List[Any]) -> List[T]:
-        db_items = session.query(orm_obj_type).filter(
-            getattr(orm_obj_type, attr_name).in_(attr_values)).all()
+    async def get_items_by_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_values: List[Any]) -> List[T]:
+        q = select(orm_obj_type).where(getattr(orm_obj_type, attr_name).in_(attr_values))
+        result = await session.execute(q)
+        result = [t[0] for t in result.all()]
         
-        logger.info(f"{len(db_items)=}")
-
-        db_items = rearrange_items(attr_values, db_items, attr_name, True)
+        db_items = rearrange_items(attr_values, result, attr_name, True)
         return db_items
 
-    @add_session
-    def get_items_by_same_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_value: Any, sort_key: str = None) -> List[T]:
-        db_filter = session.query(orm_obj_type).filter(
-            getattr(orm_obj_type, attr_name) == attr_value)
+    async def get_items_by_same_attr(self, session: Session, orm_obj_type: Type[T], attr_name: str, attr_value: Any, sort_key: str = None) -> List[T]:
+        q = select(orm_obj_type).where(getattr(orm_obj_type, attr_name) == attr_value)
 
         if sort_key is not None:
-            db_items = db_filter.order_by(
-                getattr(orm_obj_type, sort_key)).all()
-        else:
-            db_items = db_filter.all()
+            q = q.order_by(getattr(orm_obj_type, sort_key))
+        
+        result = await session.execute(q)
+        result = [t[0] for t in result.all()]
 
-        return db_items
+        return result
 
-    @add_session
-    def get_items_of_obj(self, session: Session, orm_obj_type: Type[T], obj_id: int, attr_name: str) -> List[T]:
-        db_obj = session.query(orm_obj_type).filter(
-            orm_obj_type.id == obj_id).first()
+    async def get_items_of_obj(self, session: Session, orm_obj_type: Type[T], obj_id: int, attr_name: str) -> List[T]:
+        db_obj = await self.get_item_by_id(session, orm_obj_type, obj_id, attr_name)
         return getattr(db_obj, attr_name)
 
-    def item_obj_iteraction(self, session: Session, orm_obj_type: Type[T],
+    async def item_obj_iteraction(self, session: Session, orm_obj_type: Type[T],
                             orm_item_type: Type[T],
-                            obj_id: int, item_id: int, func: Callable, **kwargs):
-        def work(session):
-            db_obj = session.query(orm_obj_type).get(obj_id)
-            db_item = session.query(orm_item_type).get(item_id)
+                            obj_id: int, item_id: int, func, **kwargs):
+        db_obj = await self.get_item_by_id(session, orm_obj_type, obj_id)
+        db_item = await self.get_item_by_id(session, orm_item_type, item_id)
 
-            if not db_obj or not db_item:
-                return False
+        if not db_obj or not db_item:
+            return False
+        
 
-            return func(session, db_obj, db_item, **kwargs)
-        if session is None:
-            with self.database.session() as session:
-                return work(session)
-        return work(session)
+        return await func(session, db_obj, db_item, **kwargs)
 
-    @add_session
-    def add_item_to_obj(self, session: Session, orm_obj_type: Type[T], orm_item_type: Type[T],
+    async def add_item_to_obj(self, session: Session, orm_obj_type: Type[T], orm_item_type: Type[T],
                         obj_id: int, item_id: int, attr_name: str) -> bool:
-        def append_item(session, db_obj, db_item):
+        async def append_item(session, db_obj, db_item):
+            db_obj = await self.get_item_by_id(session, orm_obj_type, db_obj.id, attr_name)
             if not db_item in getattr(db_obj, attr_name):
                 getattr(db_obj, attr_name).append(db_item)
-                session.commit()
-                return True
-            return False
-        return self.item_obj_iteraction(session, orm_obj_type, orm_item_type, obj_id, item_id, append_item)
+                await session.commit()
+                return db_obj
+            return None
+            
+        return await self.item_obj_iteraction(session, orm_obj_type, orm_item_type, obj_id, item_id, append_item)
 
-    @add_session
-    def remove_item_from_obj(self, session: Session, orm_obj_type: Type[T], orm_item_type: Type[T],
+    async def remove_item_from_obj(self, session: Session, orm_obj_type: Type[T], orm_item_type: Type[T],
                              obj_id: int, item_id: int, attr_name: str) -> bool:
-        def remove_item(session, db_obj, db_item):
+        async def remove_item(session, db_obj, db_item):
+            db_obj = await self.get_item_by_id(session, orm_obj_type, db_obj.id, attr_name)
             if db_item in getattr(db_obj, attr_name):
                 getattr(db_obj, attr_name).remove(db_item)
-                session.commit()
-                return True
-            return False
-        return self.item_obj_iteraction(session, orm_obj_type, orm_item_type, obj_id, item_id, remove_item)
+                await session.commit()
+                return db_obj
+            return None
+        return await self.item_obj_iteraction(session, orm_obj_type, orm_item_type, obj_id, item_id, remove_item)
 
-    @add_session
-    def create_obj(self, session: Session, orm_obj_type: Type[T], **kwargs) -> T:
+    async def create_obj(self, session: Session, orm_obj_type: Type[T], **kwargs) -> T:
         db_item = orm_obj_type(**kwargs)
         session.add(db_item)
-        session.commit()
-        session.refresh(db_item)
+        await session.commit()
         return db_item
 
-    @add_session
-    def bulk_create_objs(self, session: Session, orm_obj_type: Type[T], items: List[Dict]) -> bool:
+    async def bulk_create_objs(self, session: Session, orm_obj_type: Type[T], items: List[Dict]) -> List[T]:
         logger.info(f"Going to bulk create {len(items)} items")
         result = [orm_obj_type(**item) for item in items]
-        session.bulk_save_objects(result)
-        session.commit()
-        return True
+        session.add_all(result)        
+        await session.commit()
+        return result
 
-    def bulk_create_objs_with_unique_key(self, orm_obj_type: Type[T], items: List[Dict], unique_key: str, update_attrs: List[str] = {}) -> bool:
+    async def bulk_create_objs_with_unique_key(self, session: Session, orm_obj_type: Type[T], items: List[Dict], unique_key: str, update_attrs: List[str] = {}) -> bool:
         """Bulk create jobs, but check if a value exsists for a unique key"""
-        db_items = self.get_items_by_attr(
+        db_items = await self.get_items_by_attr(session,
             orm_obj_type, unique_key, [item[unique_key] for item in items])
 
         existing_unique_keys = set(getattr(db_item, unique_key)
                                    for db_item in db_items)
 
-        if update_attrs:
-            existing_items = {
-                item[unique_key]: item for item in items if item[unique_key] in existing_unique_keys}
-            db_items = {getattr(db_item, unique_key)
-                                : db_item for db_item in db_items}
+        # if update_attrs:
+        #     existing_items = {
+        #         item[unique_key]: item for item in items if item[unique_key] in existing_unique_keys}
+        #     db_items = {getattr(db_item, unique_key)
+        #                         : db_item for db_item in db_items}
 
-            with self.database.session() as session:
-                logger.info(
-                    f"going to update existing items for {update_attrs}")
+        #     async with self.database.session() as session:
+        #         async with session.begin():
+        #             logger.info(
+        #                 f"going to update existing items for {update_attrs}")
 
-                [self._update_object(session, orm_obj_type, db_items[item_key].id, **{attr_name: item[attr_name] for attr_name in update_attrs})
-                 for item_key, item in existing_items.items()]
+        #             [self._update_object(session, orm_obj_type, db_items[item_key].id, **{attr_name: item[attr_name] for attr_name in update_attrs})
+        #             for item_key, item in existing_items.items()]
 
         # filter out items that already exist
         items = [item for item in items if not item[unique_key]
                  in existing_unique_keys]
-        return self.bulk_create_objs(orm_obj_type, items)
+        return await self.bulk_create_objs(session, orm_obj_type, items)
 
-    def _update_object(self, session: Session, orm_obj_type: Type[T], id: int, **kwargs) -> bool:
-        db_item = session.query(orm_obj_type).get(id)
+    async def _update_object(self, session: Session, orm_obj_type: Type[T], id: int, **kwargs) -> Union[T, None]:
+        q = select(orm_obj_type).where(getattr(orm_obj_type, "id") == id)
+        db_item = (await session.execute(q)).one()[0]
+        logger.info(db_item)
         if not db_item:
-            return False
+            return None
         for key, value in kwargs.items():
             if not hasattr(db_item, key):
-                return False
+                return None
             setattr(db_item, key, value)
-        session.commit()
-        return True
+        await session.commit()
+        return db_item
 
-    @add_session
-    def update_object(self, session: Session, orm_obj_type: Type[T], id: int, **kwargs) -> bool:
-        return self._update_object(session, orm_obj_type, id, **kwargs)
+    async def update_object(self, session: Session, orm_obj_type: Type[T], id: int, **kwargs) -> Union[T, None]:
+        return await self._update_object(session, orm_obj_type, id, **kwargs)
--- webapp/services/database.py
+++ webapp/services/database.py
@@ -1,10 +1,11 @@
 """Database module."""
 
-from contextlib import contextmanager, AbstractContextManager
+from contextlib import AbstractContextManager, asynccontextmanager
 from typing import Callable
 import logging
 
-from sqlalchemy import create_engine, orm
+from sqlalchemy import orm
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
@@ -18,27 +19,28 @@ class Database:
 
     def __init__(self, db_url: str) -> None:
         self.db_url = db_url
-        self._engine = create_engine(db_url, echo=False)
-        self._session_factory = orm.scoped_session(
-            orm.sessionmaker(
+        logger.info(db_url)
+        self._engine = create_async_engine(db_url, echo=False)
+        self._session_factory = orm.sessionmaker(
                 autocommit=False,
                 autoflush=False,
                 bind=self._engine,
-            ),
-        )
+                expire_on_commit=False,
+                class_=AsyncSession
+            )
 
-    def create_database(self) -> None:
-        print("creating database")
-        Base.metadata.create_all(self._engine)
+    async def create_database(self) -> None:
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
-    @contextmanager
-    def session(self) -> Callable[..., AbstractContextManager[Session]]:
+    @asynccontextmanager
+    async def session(self) -> Callable[..., AbstractContextManager[Session]]:
         session: Session = self._session_factory()
         try:
             yield session
         except Exception:
             logger.exception("Session rollback because of exception")
-            session.rollback()
+            await session.rollback()
             raise
         finally:
-            session.close()
+            await session.close()
